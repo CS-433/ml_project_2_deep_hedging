d98a33c4c91e4949ea329090f7c5067551d9548c
qraft-kibeom <kibeom.kim@qraftec.com>
2022-12-16 23:27:07 +0900

    hyperparam tuning.py update (only train lrs)

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -7,7 +7,7 @@ from src.agent import DDPG_Hedger
 from src.network import MLP
 
 BATCH_SIZE = 16
-N_EPISODE = 5000
+N_EPISODE = 500
 
 
 def objective(trial):
@@ -15,16 +15,14 @@ def objective(trial):
     # set optuna param range
     critic_lr = 10 ** trial.suggest_float("critic_lr", -6, -1)
     actor_lr = 10 ** trial.suggest_float("actor_lr", -6, -1)
-    nHidden = trial.suggest_int("hidden_dim", 4, 32)
-    trg_update = trial.suggest_int("polyak_update_freq", 5, 20)
 
     # define environment and the agent
     env = StockTradingEnv(reset_path=True)
     nState, nAction = env.observation_space.shape[0], env.action_space.shape[0]  # 3, 1
 
-    actor = MLP(nState, nHidden, nAction, "Sigmoid")
-    qnet_1 = MLP(nState + nAction, nHidden, nAction, "")
-    qnet_2 = MLP(nState + nAction, nHidden, nAction, "")
+    actor = MLP(nState, 32, nAction, "Sigmoid")
+    qnet_1 = MLP(nState + nAction, 32, nAction, "")
+    qnet_2 = MLP(nState + nAction, 32, nAction, "")
     agent = DDPG_Hedger(actor, qnet_1, qnet_2, actor_lr, critic_lr, 1, BATCH_SIZE)
 
     target_rewards = []
@@ -47,7 +45,9 @@ def objective(trial):
 
             ep_tot_reward -= reward
             state = next_state
+
             agent.update()
+            agent.polyak_update()
 
             if done:
                 break
@@ -56,9 +56,6 @@ def objective(trial):
         # we only consider alst 10 total rewards as a quantity to minimize
         if episode > N_EPISODE - 1000:
             target_rewards.append(ep_tot_reward)
-
-        if episode % trg_update == 0:  # update target network
-            agent.polyak_update()
 
         noise_std *= 0.9999
 
